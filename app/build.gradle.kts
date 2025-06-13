4fda16b3766bef485a8b0c36e516c0b435fdcc24
Jhon-boop-alt <2201010335@undc.edu.pe>
2025-06-12 22:53:21 -0500

    Version 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.registroeventosethel"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.jhonboop.eventosethel"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
